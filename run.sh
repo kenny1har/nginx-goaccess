58a117c448643812ccbd2585be55ee4548c3797e
Kenny Hartono <kenny1har@yahoo.com>
2017-08-09 10:53:57 +0000

    port

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup http-server -p 8082 /app 2> foo.err < /dev/null &
+nohup http-server -p 80 /app 2> foo.err < /dev/null &
 { zcat -f /var/log/nginx/*access* && tail -f /var/log/nginx/*access*.log; } | goaccess --port 8081 -o /app/index.html --real-time-html
