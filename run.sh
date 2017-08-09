7b5966ae21f11873634a5aec6d46e633fe4ceae7
Kenny Hartono <kenny1har@yahoo.com>
2017-08-09 11:04:31 +0000

    loop

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 nohup http-server -p 80 /app 2> foo.err < /dev/null &
-{ zcat -f /var/log/nginx/*access* && tail -f /var/log/nginx/*access*.log; } | goaccess --port 8081 -o /app/index.html --real-time-html
+while true
+do
+  { zcat -f /var/log/nginx/*access* && tail -f /var/log/nginx/*access*.log; } | goaccess --port 8081 -o /app/index.html --real-time-html
+  sleep 2
+done
