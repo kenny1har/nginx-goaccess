b10d617931201816d4ba81bd7462b2a42b8f33dc
Kenny Hartono <kenny1har@yahoo.com>
2017-08-11 03:19:52 +0000

    change tail to multitail

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 nohup http-server-with-auth /app -p 80 -g --username goaccess --password goaccess 2> foo.err < /dev/null &
 while true
 do
-  { zcat -f /var/log/nginx/*access* && tail -f /var/log/nginx/*access*.log; } | goaccess --port 8081 -o /app/index.html --real-time-html
+  { zcat -f /var/log/nginx/*access* && multitail -Q 900 '/var/log/nginx/*access*.log'; } | goaccess --port 8081 -o /app/index.html --real-time-html
   sleep 2
 done
